a9f324ccfbdb7639316808c4f1d256b978fc4980
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-01-24 13:34:29 +0530

    Change license to LGPL

--- handler/handle.py
+++ handler/handle.py
@@ -1,18 +1,19 @@
 #  Copyright (C) 2016 Muhammed Shamil K
-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#This file is part of nIRC.
 #
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, download it from here: https://noteness.cf/GPL.txt
-#  PDF: https://noteness.cf/GPL.pdf
+#nIRC is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    nIRC is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with nIRC.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 from collections import defaultdict
